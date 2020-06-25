798c8b86b32d78d2f62225b73956aff85fda994d
Gary <garyguan0713@gmail.com>
2020-06-24 22:31:50 -0700

    Updates

---
+++ precache-manifest.352708b41d36223e3de0559780b72bcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250319120255d35b86b6d2d78747a89a",
+    "url": "/space-x-client/index.html"
+  },
+  {
+    "revision": "6fbeaf58ae63809938fc",
+    "url": "/space-x-client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b5cd9b0d02ac16dfdf2",
+    "url": "/space-x-client/static/css/main.459770b5.chunk.css"
+  },
+  {
+    "revision": "6fbeaf58ae63809938fc",
+    "url": "/space-x-client/static/js/2.7496d5ad.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/space-x-client/static/js/2.7496d5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5cd9b0d02ac16dfdf2",
+    "url": "/space-x-client/static/js/main.8268996a.chunk.js"
+  },
+  {
+    "revision": "5380c4940e63910c7436",
+    "url": "/space-x-client/static/js/runtime-main.b7c6e54d.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/space-x-client/static/media/logo.7dab9996.png"
+  }
+]);
